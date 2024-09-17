8f02e16eb517b40feb1d22c781976914421acb72
Kelvin Yeung <kelvinyg@bu.edu>
2024-09-17 01:26:06 -0400

    Procfile and added wilcard

--- cs412/settings.py
+++ cs412/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-5$thetf9f%&)4xx%u$_ut9fbd+k%1=5k6y2378w4(nbj)07b03
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
