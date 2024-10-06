7f35247d78023d15ab2c951ad4d81c810e176b5e
Kelvin Yeung <kelvinyg@bu.edu>
2024-10-06 04:12:42 -0400

    changed template directory

--- mini_fb/views.py
+++ mini_fb/views.py
@@ -3,5 +3,5 @@ from .models import Profile
 
 class ShowAllProfilesView(ListView):
     model = Profile
-    template_name = 'show_all_profiles.html'
+    template_name = 'mini_fb/show_all_profiles.html'  # Correct template path
     context_object_name = 'profiles'
